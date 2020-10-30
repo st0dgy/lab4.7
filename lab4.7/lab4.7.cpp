859d12e0668146509fdb35264c2f44fd4cbd869f
st0dgy <72568988+st0dgy@users.noreply.github.com>
2020-10-30 15:12:04 +0200

    Add files via upload

---
+++ lab4.7/lab4.7.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+    int n = 0;
+
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "eps = "; cin >> eps;
+
+    cout << fixed;
+
+    cout << "-------------------------------------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(10) << "log(x)" << " |"
+        << setw(9) << "S" << " |"
+        << setw(6) << "n" << " |"
+        << endl;
+    cout << "-------------------------------------------------" << endl;
+
+    x = xp;
+    while (x <= xk)
+    {
+        n = 0;
+        a = x - 1;
+        S = a;
+        do {
+            n++;
+            R = (1 - x) * (n / (1. * n + 1));
+            a *= R;
+            S += a;
+        } while (abs(a) >= eps);
+
+        cout << "|" << setw(7) << setprecision(2) << x << " |"
+            << setw(10) << setprecision(5) << log(x) << " |"
+            << setw(10) << setprecision(5) << S << " |"
+            << setw(5) << n << " |"
+            << endl;
+        x += dx;
+    }
+    cout << "-------------------------------------------------" << endl;
+
+
+    return 0;
+}
